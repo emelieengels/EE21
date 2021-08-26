37d265668085d01cffc66699b27f580f2667b8af
Emelie Engels <emelie@engelsmail.com>
2021-08-26 12:17:51 +0100

    logging hinzugefügt, orders updated

--- src/orderbook.py
+++ src/orderbook.py
@@ -3,21 +3,31 @@ import random
 import optparse
 import logging
 
-logging.basicConfig(filename="logfilename.log")#, level=logging.DEBUG)
-logging.debug('This is a debug message')
-logging.info('This is an info message')
-logging.warning('This is a warning message')
-logging.error('This is an error message')
-logging.critical('This is a critical message')
-
 def timestamp():
     return datetime.now()
 
+#logging.basicConfig()
+
+#log = logging.getLogger('EmsOrderbook')
+#log.info('Starting orderbook')
+#try:
+    #emsmodule.DoIt()
+#except Exception as e:
+    #log.exception('There was a problem.')
+#log.info('Ending app')
+
+logging.basicConfig(filename="emelieslogs.log")
+#logging.debug('This is a debug message')
+logging.info('Program is starting')
+#logging.warning('This is a warning message')
+#logging.error('This is an error message')
+#logging.critical('This is a critical message')
+
 def createOrder():
-    return Order(random.choice('BS'), random.randint(1, 10), random.randint(115, 130) , timestamp())
+    return Order(random.choice('BS'), random.randint(1, 10), random.randint(115, 130), timestamp())
 
 class Order(object):
-    def __init__(self, side, qty, price, timestamp):  # -> object:
+    def __init__(self, side, qty, price, timestamp):
         self.Side = side
         self.Qty = qty
         self.Price = price
@@ -42,26 +52,32 @@ class Pricelevel(object):
     def AddOrder(self, order):
         if order.Timestamp in self.Orders:
             raise Exception("order exist already with timestamp %s" % str(order.Timestamp))
+            logging.error('This order exist already')
         self.Orders[order.Timestamp] = order
 
     def DelOrder(self, order):
         if order.Timestamp not in self.Orders:
             raise Exception('order does not exist with timestamp %s' % str(order.Timestamp))
+            logging.error('This order does not exist')
         del self.Orders[order.Timestamp]
 
-    def DelPricelevel(self, order):
-        if order.Timestamp not in self.Orders:
-            raise Exception('Pricelevel does not exist with timestamp %s' % str(order.Timestamp))
-        del self.Pricelevel[order.Timestamp]
+    def IsEmpty(self):
+        if self.Orders == {}:
+            print('this dictionary is empty:')
+            return True
+        else:
+            print('this dictionary is empty:')
+            return False
 
-    #def UpdateOrder(self, order):
+    def GetNumberOrder(self):
+        print('number of oders in this pricelevel is: ', len(self.Orders.keys()))
 
 
 class OrderbookSide(object):
 
     def __init__(self, side):
         self.Side = side
-        self.Pricelevels = {}  # Price : PriceLevel
+        self.Pricelevels = {}  #Price : PriceLevel
 
     def __str__(self):
         txt = '\nOrderbook Side ' + self.Side
@@ -76,31 +92,27 @@ class OrderbookSide(object):
             self.Pricelevels[order.Price] = priceLevel
             temp = {}
             priceLevels = sorted(self.Pricelevels.keys(), reverse=True) #buyside high to low
-            #priceLevels = sorted(self.Pricelevels.keys()) #sellside low to high
             for pl in priceLevels:
                 temp[pl] = self.Pricelevels[pl]
             self.Pricelevels = temp
-            #sorted(self.Pricelevels, reverse=True)
+            logging.info('Pricelevels have been added')
         else:
-            priceLevel = self.Pricelevels[order.Price]
+            priceLevel = self.Pricelevels[order.Price] #key von pricelevel dict is order.Price
         priceLevel.AddOrder(order)
+        logging.info('Order has been added')
 
     def DelOrder(self, order):
         if order.Price in self.Pricelevels:
-            priceLevel = self.Pricelevels[order.Price] #and then delete pricelevel as order gets deleted
+            priceLevel = self.Pricelevels[order.Price]
+            del self.Pricelevels[order.Price] #key von price level: del[] keys der pricelevel zum rauslöschen, ein dictionary eintrag löschen- del[keyname der löschung]
+            logging.info('Pricelevel has been deleted')
         else:
             raise Exception('Price level does not exist : %s' % str(order.Price))
-            #priceLevel = self.Pricelevels[order.Price]
+            logging.error('This pl does not exist')
         priceLevel.DelOrder(order)
-
-    def DelPricelevel(self, order):
-        if order.Price not in self.Pricelevels:
-            priceLevel = self.Pricelevels[order.Price]
-            priceLevel.DelPricelevel(order)
-        else:
-            pass
-
-    #def UpdateOrder(self, order):
+        logging.info('Order has been deleted')
+        #print(priceLevel.IsEmpty())
+        #print(priceLevel.GetNumberOrder())
 
 
 class Orderbook(object):
@@ -108,7 +120,6 @@ class Orderbook(object):
     def __init__(self):
         self.BuySide = OrderbookSide('B')
         self.SellSide = OrderbookSide('S')
-        #self.SellSide = sorted(self.SellSide.Pricelevels.keys(), reverse=True)
 
     def __str__(self):
         txt = '\n\n>>>>>>>>>>Orderbook\n'
@@ -122,7 +133,6 @@ class Orderbook(object):
         if order.Side == 'B':
             self.BuySide.AddOrder(order)
         elif order.Side == 'S':
-            #sorted(self.SellSide, reverse=True)
             self.SellSide.AddOrder(order)
         else:
             raise Exception("illegal Side: %s" % order.Side)
@@ -135,58 +145,38 @@ class Orderbook(object):
         else:
             raise Exception('illegal Side: %s' % order.Side)
 
-    def DelPricelevel(self, order):
-        if order.Side == 'B':
-            self.BuySide.DelPricelevel(order)
-        elif order.Side == 'S':
-            self.SellSide.DelPricelevel(order)
-        else:
-            raise Exception('illegal Side: %s' % order.Side)
-
-    def UpdateOrder(self, order):
-        if order.Side == 'B':
-            self.BuySide.UpdateOrder(order)
-        elif order.Side == 'S':
-            self.SellSide.UpdateOrder(order)
-        else:
-            raise Exception('illegal Side: %s' % order.Side)
-
-    def PrintMe(self):
-        for order in self.orders:
-            print(order.__str__())  # transforms layout of outcome of orderbook in line and with spaces, ToString is defined in the python file order.py, which is still being used in this orderbook
-
-
 if __name__ == '__main__':
     parser = optparse.OptionParser("usage: %prog [options] marke zdr farbe") #example of how to use optparse
     parser.add_option("-o", "--ordercount", dest="ordercountnumber", default = 0, type = "int", help = "specify ordercount")
     options, args = parser.parse_args()
-    #options.ordercountnumber = None
     print('options:', options)
     print('args:', args)
-    #if len(args) != 3:
-        #parser.error("incorrect number of arguments")
     print(options.ordercountnumber)
 
     orderbook = Orderbook()
+    logging.info('Orderbook has been created')
 
     try:
         order1 = createOrder()
-        order2 = createOrder()
-        print(order2)
+        #order2 = createOrder()
+        order2 = Order('B', 2, 120, timestamp())
+        order2a = Order('B', 3, 121, timestamp())
+        order2b = Order('B', 4, 121, timestamp())
         order3 = createOrder()
         order4 = createOrder()
         order5 = createOrder()
         order6 = createOrder()
         orderbook.AddOrder(order1)
         orderbook.AddOrder(order2)
+        orderbook.AddOrder(order2a)
+        orderbook.AddOrder(order2b)
         orderbook.AddOrder(order3)
         orderbook.AddOrder(order4)
         orderbook.AddOrder(order5)
         orderbook.AddOrder(order6)
         orderbook.DelOrder(order2)
         print(orderbook)
-
-        #orderbook.DelOrder(order1)
-        #print(orderbook)
     except Exception as info:
         raise
+
+    logging.info('Program is ending')
